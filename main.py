954c9c8f479437791547e6af43f2b13e98ca862a
StreetDreaminn <64617474+StreetDreaminn@users.noreply.github.com>
2023-06-07 22:53:42 +0100

    Initial upload of main file.

---
+++ main.py
@@ -0,0 +1,2 @@
+# The purpose of this script is to read a file and generate the total number of times a word appears in the file.
+# It should accept a file as an input and the output should be a number
